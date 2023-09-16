1bfb7724d7f82d6bd476feae46c2c637c35ec275
AndyCoding02 <drewazarov26@gmail.com>
2023-09-16 16:22:58 +0300

    update

--- src/server.js
+++ src/server.js
@@ -22,7 +22,6 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
-
 app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
